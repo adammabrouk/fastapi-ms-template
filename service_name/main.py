5115ffb43cf8fe9248656cc7e3524f80d1203e9f
Adam Mabrouk <mabrouk.adam@outlook.com>
2025-05-20 00:55:07 +0200

    Fixing Isort

--- service_name/main.py
+++ service_name/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI(title="Service API", version="1.0.0")
 
 # CORS middleware to allow requests from the frontend
